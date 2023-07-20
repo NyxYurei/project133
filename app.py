ee3636fcf2e1f18c41217f024bcd58d80678f6e7
Nyx Yürei <97993260+NyxYurei@users.noreply.github.com>
2023-07-20 18:02:29 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+# importando os módulos da biblioteca flask
+from flask import Flask, render_template
+
+# criando a instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+app = Flask(__name__)
+
+
+# escreva as rotas usando funções de decorador
+# rota padrão ou 'URL'
+@app.route("/")
+def home():
+    nome = "Ana Clara"  # escreva seu nome
+    idade = "15"  # escreva sua idade
+
+    return render_template('index.html', nome=nome, idade=idade)
+
+
+# defina a rota para a página do pai
+
+@app.route("/pai")
+def pai():
+    nome = "Douglas"
+    idade = "44"
+
+    return render_template('pai.html', nome=nome, idade=idade)
+
+
+# defina a rota para a página da mãe
+
+@app.route("/mae")
+def mae():
+    nome = "Raquel"
+    idade = "40"
+
+    return render_template('mae.html', nome=nome, idade=idade)
+
+
+# defina a rota para a página do amigo
+
+@app.route("/amigo")
+def amigo():
+    nome = "A"
+    idade = "15"
+
+    return render_template('amigo.html', nome=nome, idade=idade)
+
+
+# adicione outras rotas, se você quiser
+
+@app.route("/pet")
+def pet():
+    nome = "Belinha"
+    idade = "5"
+
+    return render_template('pet.html', nome=nome, idade=idade)
+
+
+# execute o arquivo
+if __name__ == '__main__':
+    app.run(debug=True)
